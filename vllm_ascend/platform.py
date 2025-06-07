c58f7f760b6ad9ec31c768026d39e2dfe950dc9e
Vincent Yuan <farawayboat@gmail.com>
2025-06-07 08:45:04 +0000

    feat: support v1 engine on 310P
    Signed-off-by: Vincent Yuan <farawayboat@gmail.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -28,7 +28,8 @@ from vllm.logger import logger
 from vllm.platforms import Platform, PlatformEnum
 
 from vllm_ascend.ascend_config import check_ascend_config, init_ascend_config
-from vllm_ascend.utils import ASCEND_QUATIZATION_METHOD, update_aclgraph_sizes
+from vllm_ascend.utils import (ASCEND_QUATIZATION_METHOD, is_310p,
+                               update_aclgraph_sizes)
 
 CUSTOM_OP_ENABLED = False
 try:
